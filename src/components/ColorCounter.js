b26297afdf987dbd56771ea85d21554360b4f39f
blaisebakundukize <blaisebakundukize@gmail.com>
2020-01-03 11:34:25 +0200

    added react-native app examples

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
+  // const [colors, setColors] = useState([]);
+
+  return (
+    <View>
+      <Text>{color}</Text>
+      <Button
+        title={`Increase ${color}`}
+        onPress={() => onIncrease()}
+      />
+      <Button
+        title={`Decrease ${color}`}
+        onPress={() => onDecrease()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorCounter;
